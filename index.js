ad7ccf1ddeea68d8eb7ba1cfe309227f89b932ad
MUHAMMAD MUZZAMIL <muzammilmuhammad196@gmail.com>
2023-07-04 18:00:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 connectToMongo();
 
@@ -19,6 +20,11 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+// static files
+app.use(express.static(path.join(__dirname, './build')));
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`INotebook app listening on port ${port}`)
